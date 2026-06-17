3bc4ced3a243fc31095db4617178dbf64d608bb6
Diego Sagredo <diegsagdemig@hotmail.com>
2026-06-17 17:07:10 +0200

    Changed the attach_to for bind_to as it was needed

--- src/application.h
+++ src/application.h
@@ -20,7 +20,7 @@ class HairViewer
     // Single source of truth for the loaded scene. Referenced by both
     // init() (to peek renderer.msaa before constructing the renderer) and
     // setup() (to actually load the scene).
-    static constexpr const char* SCENE_PATH = RESOURCES_PATH "scenes/nadia.json";
+    static constexpr const char* SCENE_PATH = RESOURCES_PATH "scenes/maria.json";
 
     UserInterface m_interface{};
 
